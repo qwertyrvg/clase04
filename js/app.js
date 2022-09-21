3fdb4dd013e03eed738dac7884cc16a0312f4dd6
SeeR <seer.cs@gmail.com>
2022-09-21 19:39:54 -0300

    classes

---
+++ js/app.js
@@ -0,0 +1,33 @@
+class Boton {
+    constructor(display, color, padding, text-decoration) {
+        this._display = display,
+            this.color = color,
+            this.text-decoration = text-decoration;
+    }
+}
+
+let boton1 = new Boton
+
+class de Video {
+    constructor(description, width, height) {
+            this.description = description,
+            this.width = width,
+            this.height = height,
+    }
+}
+
+class de Audio {
+    constructor(title, url) {
+        this.title = title,
+            this.url = url,
+    }
+}
+
+class de List {
+    constructor(elements, styles, className) {
+        this.className = className,
+        this.elements = elements,
+    }
+}
+
+
